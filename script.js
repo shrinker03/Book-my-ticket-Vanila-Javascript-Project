eb3f5b485756a4be5f8b9926d4b44b40b32c15cd
shrinker03 <48637826+shrinker03@users.noreply.github.com>
2020-08-04 17:48:48 +0530

    First push of the project to the github

---
+++ script.js
@@ -0,0 +1,75 @@
+
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.ocuupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+let ticketPrice = +movieSelect.value;
+
+populateUI()
+
+//Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
+
+
+// Update count and total
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+    const selectedSeatsCounts = selectedSeats.length;
+
+    count.innerText = selectedSeatsCounts;
+    total.innerText = selectedSeatsCounts * ticketPrice;
+}
+
+
+//Get data from local storage and populate UI
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+    if(selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if(selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            }
+        });
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex')
+
+    if(selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+}
+
+
+//Movie select event
+movieSelect.addEventListener('change', e => {
+    ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value)
+    updateSelectedCount()
+})
+
+
+// Seat click event
+container.addEventListener('click', e => {
+    if(
+        e.target.classList.contains('seat') && 
+        !e.target.classList.contains('occupied')
+        ){
+            e.target.classList.toggle('selected');
+
+            updateSelectedCount();
+        }  
+});
+
+//Initial count and total set
+updateSelectedCount();
